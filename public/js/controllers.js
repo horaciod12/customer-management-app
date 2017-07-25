eded8e1d580e67382d998441b7f9111ed86271b4
horaciod12 <daddarioh@gmail.com>
2017-07-25 18:54:14 -0300

    adding styles to the nav menu

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -39,3 +39,13 @@ app.controller('editCtrl', function($scope, $routeParams, CustomersApp, $window)
 	}
 		
 })
+
+app.controller('NavbarController', function ($scope, $location) {
+    $scope.getClass = function (path) {
+        if ($location.path().substr(0, path.length) == path) {
+            return true
+        } else {
+            return false;
+        }
+    }
+})
--- public/js/directives.js
+++ public/js/directives.js
@@ -1,6 +1,7 @@
 app.directive('navMenu', function(){
 	return {
 		restrict: 'E',
-		templateUrl: 'views/nav.html'
+		templateUrl: 'views/nav.html',
+		controller: 'NavbarController'
 	}
 })
